39ef11007c30d04ee0c2b3c6a1a77195cafabbfa
Rachel Byrne <rachelbyrne1994@hotmail.com>
2016-10-14 10:31:52 +0100

    Lab 4: Pipe files

---
+++ Week4/cntlm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Prints out information on the cntlm process running
+ps -ef | grep cntlm | grep root
---
+++ Week4/pipe.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#comment what this new command achieves
+#shows all active activities
+#ps reports a snapshot of the current processes
+#To see every processes on the systems
+
+#grep mysql searches for the word mysql
+#searchs for all processes that contains mysql string
+ps -ef | grep mysql
+
+
+
+
+
